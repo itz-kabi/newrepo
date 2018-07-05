926319083c8ce2f5884db9caa99f643b8a9299e4
IBM <IBM@IBM-FULLSTACK7.mshome.net>
2018-07-05 17:11:53 +0530

    hii

---
+++ kabi/src/test/java/kabi/StringCalculatorTest.java
@@ -0,0 +1,20 @@
+package kabi;
+
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+	@Test
+	public void testAdd() {
+
+		StringCalculator.add("1,2");
+
+
+	}   
+//	@Test (expected= RuntimeException.class)
+//	public void testIsNumberLengthGreaterThanTwo(){
+//		StringCalculator.add("x,3");
+//	}
+}
+
+
